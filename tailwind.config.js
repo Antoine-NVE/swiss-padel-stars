9c375705aa0d943fbd4b20d05a38766363da36b1
razmiO <thomas.cuesta31@gmail.com>
2024-12-11 15:25:10 +0100

    feat : init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html.twig", "./assets/**/*.tsx", "./assets/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
