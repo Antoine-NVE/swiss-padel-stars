c402b03d752d7b2bebae43197edd79ed2b0998e0
Antoine-NVE <antoinenavette@gmail.com>
2024-12-16 12:05:39 +0100

    refactor: move register function to authcontroller

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -11,9 +11,44 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AuthController extends AbstractController
 {
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        ValidatorInterface $validator
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        $email = $data['email'] ?? null;
+        $password = $data['password'] ?? null;
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+        // On vient vérifier les contraintes de validation (NotBlank, Length, UniqueEntity, etc.)
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+            return new JsonResponse(['errors' => $errorMessages], 400);
+        }
+
+        $user->setPassword($passwordHasher->hashPassword($user, $password));
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Inscription réussie !'], 201);
+    }
+
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function login(
         Request $request,
