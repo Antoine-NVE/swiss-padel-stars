7b25e3de71e19d7dda86209baf49067cdbae0b4b
razmiO <thomas.cuesta31@gmail.com>
2024-12-17 16:29:02 +0100

    feat : integrate auth

--- assets/components/HelloWorld.tsx
+++ assets/components/HelloWorld.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import ButtonUser from "./ButtonUser";
+import Form from "./Form";
 
-type HelloWorldProps = {
-    name?: string; // Exemple de propriété optionnelle
+const HelloWorld = () => {
+    return (
+        <>
+            <h1 className="text-green-500">Hello!</h1>
+            <div className="w-full flex flex-col items-center justify-center gap-3">
+                <ButtonUser />
+                <Form url="/api/login" title="LOGIN" />
+                <Form url="/api/register" title="REGISTER" />
+            </div>
+        </>
+    );
 };
 
-const HelloWorld: React.FC<HelloWorldProps> = ({ name }) => {
-    return <h1 className="text-green-500">Hello, {name || "React with TypeScript"}!</h1>;
-};
+// {
+//     "email" : "thomas.cuesta@gmail.com",
+//     "password" : "AZERQSDF"
+// }
 
+// {
+//     "message": "Inscription réussie !"
+// }
 export default HelloWorld;
