5b4db75111f6b8c929ebdd25011d8ed38321b512
Antoine NAVETTE <antoinenavette@gmail.com>
2024-12-17 16:13:05 +0100

    feat: add refresh token entity

---
+++ src/Entity/RefreshToken.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RefreshTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: RefreshTokenRepository::class)]
+#[UniqueEntity(fields: ['token'])]
+class RefreshToken
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $token = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $expiresAt = null;
+
+    #[ORM\Column]
+    private ?bool $revoked = null;
+
+    #[ORM\ManyToOne(inversedBy: 'refreshTokens')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): static
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getExpiresAt(): ?\DateTimeImmutable
+    {
+        return $this->expiresAt;
+    }
+
+    public function setExpiresAt(\DateTimeImmutable $expiresAt): static
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    public function isRevoked(): ?bool
+    {
+        return $this->revoked;
+    }
+
+    public function setRevoked(bool $revoked): static
+    {
+        $this->revoked = $revoked;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
