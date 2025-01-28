8b2550b63c8ea4f67924961a4f580e30233f77fc
Antoine-NVE <antoinenavette@gmail.com>
2025-01-28 17:49:33 +0100

    feat: change auth failure message

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,11 +6,13 @@ use App\Response\StandardJsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/api/profile', name: 'api_profile_')]
 class ProfileController extends AbstractController
 {
     #[Route('/me', name: 'me', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
     public function index(): JsonResponse
     {
         /** @var \App\Entity\User $user */
