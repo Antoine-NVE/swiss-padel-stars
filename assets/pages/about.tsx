2baa9a46fc7c91eeb98a15e50d5bb86bebacee7e
razmiO <thomas.cuesta31@gmail.com>
2025-02-04 23:12:40 +0100

    feat(front): add About and Index pages

---
+++ assets/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>This is a simple example of a React app built with Snowpack.</p>
+        </>
+    );
+}
---
+++ assets/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Index() {
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Welcome to the home page of this simple React app.</p>
+        </>
+    );
+}
