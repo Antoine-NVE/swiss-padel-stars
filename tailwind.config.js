1634db9b604cccbda74e38de42c4b00c65115e05
razmiO <thomas.cuesta31@gmail.com>
2025-02-05 00:38:48 +0100

    refactor(tailwind): update theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html.twig", "./assets/**/*.tsx", "./assets/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./templates/**/*.html.twig", "./assets/**/*.tsx", "./assets/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#093C6F",
+                secondary: "#D6AD3A",
+                dark: {
+                    primary: "#1D1D1F",
+                    secondary: "#2C2C2C",
+                },
+            },
+        },
+    },
+    plugins: [],
 };
