d8e35f556d06e5193a2641d02a359cbea0694764
razmiO <thomas.cuesta31@gmail.com>
2025-02-05 12:35:37 +0100

    feat(pages): add /infrastructure & /nous & /partenariat & /produits

---
+++ assets/pages/produits.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div className="grow">
+            <h1>Produits</h1>
+            <p>This is a simple example of a React app built with Snowpack.</p>
+        </div>
+    );
+}
