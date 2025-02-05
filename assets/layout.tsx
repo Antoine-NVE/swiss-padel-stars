1c4829eea45d79cc19eaffa30d038540742c064a
razmiO <thomas.cuesta31@gmail.com>
2025-02-05 16:52:24 +0100

    styles(layout.tsx): add relative positioning to main element

--- assets/layout.tsx
+++ assets/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <main className="w-screen flex flex-col min-h-screen">
+        <main className="relative w-screen flex flex-col min-h-screen">
             {children}
             <footer>footer</footer>
         </main>
