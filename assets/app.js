93d3f6ad60019b689ed10f7a5b6c7a919f582e97
razmiO <thomas.cuesta31@gmail.com>
2025-02-05 12:50:57 +0100

    refactor(app.js): extract header>nav from layout.tsx

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,47 @@
 //@ts-check
-import Home from "./pages/index.tsx";
-import About from "./pages/about.tsx";
+import Accueil from "./pages/index.tsx";
+import Nous from "./pages/nous.tsx";
+import Contact from "./pages/contact.tsx";
+import Infrastructure from "./pages/infrastructure.tsx";
+import Partenariat from "./pages/partenariat.tsx";
+import Produits from "./pages/produits.tsx";
 import Layout from "./layout.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./styles/app.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Nav from "./components/Nav.tsx";
 
 const links = [
     {
-        label: "Home",
+        label: "Accueil",
         href: "/",
+        component: Accueil,
     },
     {
-        label: "About",
-        href: "/about",
+        label: "Qui sommes-nous",
+        href: "/nous",
+        component: Nous,
     },
     {
         label: "Contact",
         href: "/contact",
+        component: Contact,
+    },
+    {
+        label: "Infrastructure",
+        href: "/infrastructure",
+        component: Infrastructure,
+    },
+    {
+        label: "Partenariat et sponsoring",
+        href: "/partenariat",
+        component: Partenariat,
+    },
+    {
+        label: "Produits",
+        href: "/produits",
+        component: Produits,
     },
 ];
 
@@ -32,10 +55,14 @@ if (rootElement) {
                     <Route
                         path={link.href}
                         element={
-                            <Layout links={links}>
-                                <Home />
+                            <Layout>
+                                <header className="flex items-center justify-around w-screen bg-primary py-4 px-2 absolute">
+                                    <Nav links={links} />
+                                </header>
+                                <link.component />
                             </Layout>
-                        }></Route>
+                        }
+                    />
                 ))}
             </Routes>
         </BrowserRouter>
--- assets/layout.tsx
+++ assets/layout.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import Nav from "./components/Nav";
-import type { NavLink } from "./types";
 
-export default function Layout({ children, links }: { children: React.ReactNode; links: NavLink[] }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="w-screen flex flex-col min-h-screen">
-            <header className="flex items-center justify-around w-screen bg-primary py-4 px-2">
-                <Nav links={links} />
-            </header>
             {children}
             <footer>footer</footer>
         </main>
