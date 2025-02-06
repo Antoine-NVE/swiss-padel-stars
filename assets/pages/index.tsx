7a8d2e9b25390a3a4f815469796a4bd9bc534ac3
razmiO <thomas.cuesta31@gmail.com>
2025-02-06 18:06:31 +0100

    feat(index): Add first two articles

--- assets/pages/index.tsx
+++ assets/pages/index.tsx
@@ -3,15 +3,29 @@ import { Button as RadixButton } from "../components/ui/button";
 import { type ImgProps } from "../types";
 import { cn } from "../utils";
 
-const Article = ({ img, text, direction }: { img: ImgProps; text: string; direction: "rtl" | "ltr" }) => {
+const Article = ({
+    img,
+    text,
+    direction,
+    btnText,
+}: {
+    img: ImgProps;
+    text: string;
+    direction?: "rtl" | "ltr";
+    btnText: string;
+}) => {
     return (
-        <article className="flex center justify-around [&_.wrapper]:w-[500px]">
+        <article
+            className={cn(
+                "flex center justify-around [&_.wrapper]:w-[500px]",
+                direction === "ltr" && "flex-row-reverse"
+            )}>
             <div className="wrapper">
                 <img src={img.src} alt={img.alt} width={500} height={400} className="object-cover rounded-lg" />
             </div>
             <div className="wrapper flex flex-col items-center justify-around">
                 <p className="text-white text-3xl text-balance text-justify">{text}</p>
-                <Button className="w-fit">Ghost</Button>
+                <Button className="w-fit">{btnText}</Button>
             </div>
         </article>
     );
@@ -29,14 +43,16 @@ const Hero = () => {
 };
 
 export default function Page() {
-    const articles: { img: ImgProps; text: string }[] = [
+    const articles: { img: ImgProps; text: string; btnText: string }[] = [
         {
             img: { src: "build/images/body-img-home-1.png", alt: "" },
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            btnText: "Ghost",
         },
         {
             img: { src: "build/images/body-img-home-2.png", alt: "" },
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            btnText: "Contact",
         },
     ];
 
@@ -45,8 +61,17 @@ export default function Page() {
             <Hero />
             <section className="space-y-10 w-full">
                 {articles.map((article, index) => {
+                    // présentation de gauche à droite ou de droite à gauche
                     const dir = index % 2 === 0 ? "rtl" : "ltr";
-                    return <Article direction={dir} key={index} img={article.img} text={article.text} />;
+                    return (
+                        <Article
+                            direction={dir}
+                            key={index}
+                            img={article.img}
+                            text={article.text}
+                            btnText={article.btnText}
+                        />
+                    );
                 })}
             </section>
         </div>
