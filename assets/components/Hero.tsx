23b1b6c050703c0cef4d90d90fcaacc1960b776e
razmiO <thomas.cuesta31@gmail.com>
2025-02-07 22:16:20 +0100

    styles(components): Update Hero component styling

--- assets/components/Hero.tsx
+++ assets/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = ({ title, img }: { title: string; img: Omit<ImgProps, "width
     return (
         <div className="relative">
             <img src={img.src} alt={img.alt} className="object-cover w-screen" />
-            <h1 className="absolute top-1/2 right-1/2 text-secondary text-6xl font-bold translate-x-[50%] -translate-y-[50%]">
+            <h1 className="absolute top-1/2 right-1/2 text-secondary text-6xl font-bold translate-x-[50%] -translate-y-[50%] text-center text-balance">
                 {title}
             </h1>
         </div>
