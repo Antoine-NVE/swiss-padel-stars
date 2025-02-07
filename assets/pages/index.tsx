29231afcd94d6f864a51c250c00c14a1f5769992
razmiO <thomas.cuesta31@gmail.com>
2025-02-07 21:35:29 +0100

    refactor(components): Update Page component and remove duplicate Hero component

--- assets/pages/index.tsx
+++ assets/pages/index.tsx
@@ -1,9 +1,68 @@
 import React from "react";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../components/ui/accordion";
+import CommunFaq from "../components/CommunFaq";
+import { Hero } from "../components/Hero";
 import { Button as RadixButton } from "../components/ui/button";
-import { FaqType, type ImgProps } from "../types";
+import { type ImgProps } from "../types";
 import { cn } from "../utils";
 
+export default function Page() {
+    // articles à afficher
+    const articles: { img: ImgProps | ImgProps[]; text: string; btnText: string }[] = [
+        {
+            img: { src: "build/images/body-img-home-1.png", alt: "", width: 500, height: 400 },
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            btnText: "Ghost",
+        },
+        {
+            img: { src: "build/images/body-img-home-2.png", alt: "", width: 500, height: 400 },
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            btnText: "Contact",
+        },
+        {
+            img: [
+                { src: "build/images/body-img-home-small-1.png", alt: "", width: 220, height: 220 }, //
+                { src: "build/images/body-img-home-small-2.png", alt: "", width: 220, height: 220 }, //
+            ],
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            btnText: "Commander un devis",
+        },
+    ];
+
+    return (
+        <div className="grow bg-dark-primary space-y-10">
+            <Hero title="Swiss Padel Stars" img={{ src: "/build/images/hero-home.png", alt: "banniere" }} />
+            {/* MAIN CONTENT */}
+            {/* MAIN CONTENT */}
+            {/* MAIN CONTENT */}
+            {/* MAIN CONTENT */}
+            <section className="space-y-10 w-full">
+                {articles.map((article, index) => {
+                    // présentation de gauche à droite ou de droite à gauche
+                    const dir = index % 2 === 0 ? "rtl" : "ltr";
+                    return (
+                        <Article
+                            direction={dir}
+                            key={index}
+                            img={article.img}
+                            text={article.text}
+                            btnText={article.btnText}
+                        />
+                    );
+                })}
+            </section>
+
+            {/* FAQ */}
+            {/* FAQ */}
+            {/* FAQ */}
+            {/* FAQ */}
+            <CommunFaq />
+        </div>
+    );
+}
+
+/**
+ * ui components
+ */
 type ArticleProps = {
     img: ImgProps | ImgProps[];
     text: string;
@@ -60,124 +119,6 @@ const ArticleImage = ({ img }: { img: ImgProps | ImgProps[] }) => {
     return <img {...img} className="object-cover rounded-xl" />;
 };
 
-/**
- * Hero / banniere
- */
-const Hero = () => {
-    return (
-        <div className="relative">
-            <img src="/build/images/hero-home.png" alt="" className="object-cover w-screen" />
-            <h1 className="absolute top-1/2 right-1/2 text-secondary text-6xl font-bold translate-x-[50%] -translate-y-[50%]">
-                Swiss Padel Stars
-            </h1>
-        </div>
-    );
-};
-
-export default function Page() {
-    // articles à afficher
-    const articles: { img: ImgProps | ImgProps[]; text: string; btnText: string }[] = [
-        {
-            img: { src: "build/images/body-img-home-1.png", alt: "", width: 500, height: 400 },
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-            btnText: "Ghost",
-        },
-        {
-            img: { src: "build/images/body-img-home-2.png", alt: "", width: 500, height: 400 },
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-            btnText: "Contact",
-        },
-        {
-            img: [
-                { src: "https://picsum.photos/220/220", alt: "", width: 220, height: 220 }, // build/images/body-img-home-small-1.png
-                { src: "https://picsum.photos/220/220", alt: "", width: 220, height: 220 }, // build/images/body-img-home-small-2.png
-            ],
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-            btnText: "Commander un devis",
-        },
-    ];
-
-    const faq: FaqType[] = [
-        {
-            id: "1",
-            question: "Où sont vos évènements ?",
-            answer: "Nos événements se déroulent dans divers lieux à travers la Suisse. Nous choisissons des emplacements stratégiques pour maximiser l'expérience des participants. Consultez notre calendrier pour les prochaines dates et lieux.",
-        },
-        {
-            id: "2",
-            question: "Comment installer un terrain de padel chez moi ?",
-            answer: "Nos événements se déroulent dans divers lieux à travers la Suisse. Nous choisissons des emplacements stratégiques pour maximiser l'expérience des participants. Consultez notre calendrier pour les prochaines dates et lieux.",
-        },
-        {
-            id: "3",
-            question: "Quels sont les avantages de jouer au padel ?",
-            answer: "Nos événements se déroulent dans divers lieux à travers la Suisse. Nous choisissons des emplacements stratégiques pour maximiser l'expérience des participants. Consultez notre calendrier pour les prochaines dates et lieux.",
-        },
-        {
-            id: "4",
-            question: "Comment puis-je m'inscrire à un évènement ?",
-            answer: "Nos événements se déroulent dans divers lieux à travers la Suisse. Nous choisissons des emplacements stratégiques pour maximiser l'expérience des participants. Consultez notre calendrier pour les prochaines dates et lieux.",
-        },
-        {
-            id: "5",
-            question: "Quels sont les équipements nécessaires pour jouer au padel ?",
-            answer: "Nos événements se déroulent dans divers lieux à travers la Suisse. Nous choisissons des emplacements stratégiques pour maximiser l'expérience des participants. Consultez notre calendrier pour les prochaines dates et lieux.",
-        },
-    ];
-
-    return (
-        <div className="grow bg-dark-primary space-y-10">
-            <Hero />
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
-            <section className="space-y-10 w-full">
-                {articles.map((article, index) => {
-                    // présentation de gauche à droite ou de droite à gauche
-                    const dir = index % 2 === 0 ? "rtl" : "ltr";
-                    return (
-                        <Article
-                            direction={dir}
-                            key={index}
-                            img={article.img}
-                            text={article.text}
-                            btnText={article.btnText}
-                        />
-                    );
-                })}
-            </section>
-
-            {/* FAQ */}
-            {/* FAQ */}
-            {/* FAQ */}
-            {/* FAQ */}
-            <section className="flex flex-col items-start px-36 py-6 space-y-6 bg-primary/20  text-white">
-                <p className="text-white">
-                    Découvrez les réponses aux questions fréquentes concernant nos services et événements de padel.
-                </p>
-                <Accordion type="single" collapsible className="w-full space-y-3">
-                    {faq.map((item) => (
-                        <AccordionItem key={item.id} value={item.id} className="bg-grey rounded-lg border-0 px-4">
-                            <AccordionTrigger>{item.question}</AccordionTrigger>
-                            <AccordionContent>{item.answer}</AccordionContent>
-                        </AccordionItem>
-                    ))}
-                </Accordion>
-                <div className="flex justify-center items-center gap-5 w-full">
-                    <p>Vous avez une autre question ? </p>
-                    <a href="/contact">
-                        <Button className="rounded-md">Contactez-nous</Button>
-                    </a>
-                </div>
-            </section>
-        </div>
-    );
-}
-
-/**
- * ui components
- */
 const Button = ({ children, className }: { children: React.ReactNode; className?: string }) => {
     return (
         <RadixButton
