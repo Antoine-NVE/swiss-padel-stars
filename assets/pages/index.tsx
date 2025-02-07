d6a5aa8422884320c93a1855ebbd69b9e03fe539
razmiO <thomas.cuesta31@gmail.com>
2025-02-07 22:55:54 +0100

    refactor(components): extract Article from page index

--- assets/pages/index.tsx
+++ assets/pages/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+import { Article } from "../components/Article";
 import CommunFaq from "../components/CommunFaq";
 import { Hero } from "../components/Hero";
-import { Button as RadixButton } from "../components/ui/button";
 import { type ImgProps } from "../types";
-import { cn } from "../utils";
 
 export default function Page() {
     // articles à afficher
@@ -31,10 +30,6 @@ export default function Page() {
     return (
         <div className="grow bg-dark-primary space-y-10">
             <Hero title="Swiss Padel Stars" img={{ src: "/build/images/hero-home.png", alt: "banniere" }} />
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
-            {/* MAIN CONTENT */}
             <section className="space-y-10 w-full">
                 {articles.map((article, index) => {
                     // présentation de gauche à droite ou de droite à gauche
@@ -50,81 +45,7 @@ export default function Page() {
                     );
                 })}
             </section>
-
-            {/* FAQ */}
-            {/* FAQ */}
-            {/* FAQ */}
-            {/* FAQ */}
             <CommunFaq />
         </div>
     );
 }
-
-/**
- * ui components
- */
-type ArticleProps = {
-    img: ImgProps | ImgProps[];
-    text: string;
-    direction?: "rtl" | "ltr";
-    btnText: string;
-};
-
-/**
- * Un article est un element indépendant image + texte + bouton
- * la propriété direction permet de spécifier l'ordre de présentation
- */
-const Article = ({ img, text, direction, btnText }: ArticleProps) => {
-    if (Array.isArray(img) && img.length > 2) {
-        throw new Error("More than 2 images is not implemented yet !");
-    }
-
-    return (
-        <article
-            className={cn(
-                "flex center justify-around [&_.wrapper]:w-[500px]",
-                direction === "ltr" && "flex-row-reverse"
-            )}>
-            <div className="wrapper">
-                <ArticleImage img={img} />
-            </div>
-            <div className="wrapper flex flex-col items-center justify-around">
-                <p className="text-white text-3xl text-balance text-justify">{text}</p>
-                <Button className="w-fit">{btnText}</Button>
-            </div>
-        </article>
-    );
-};
-
-/**
- * S'il y a deux images, elles sont affichées chevauchante comme le spécifie le figma
- * sinon, une seule image est affichée
- */
-const ArticleImage = ({ img }: { img: ImgProps | ImgProps[] }) => {
-    if (Array.isArray(img)) {
-        if (img.length > 2) throw new Error("More than 2 images is not implemented yet!");
-
-        return (
-            <div className="relative" style={{ height: img[0].height * 2 + "px" }}>
-                <div className="absolute top-10 right-16 z-0">
-                    <img {...img[0]} className="aspect-square object-cover rounded-xl" />
-                </div>
-                <div className="absolute bottom-10 left-16 z-10 ring-[10px] ring-dark-primary rounded-xl">
-                    <img {...img[1]} className="aspect-square object-cover rounded-xl" />
-                </div>
-            </div>
-        );
-    }
-
-    return <img {...img} className="object-cover rounded-xl" />;
-};
-
-const Button = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    return (
-        <RadixButton
-            className={cn("bg-dark-secondary text-white py-2 px-4 rounded-3xl w-fit border", className)}
-            onClick={console.log}>
-            {children}
-        </RadixButton>
-    );
-};
