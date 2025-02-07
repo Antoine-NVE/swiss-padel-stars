3cff76d41efc55560309cff32cf3db1013abd9d4
razmiO <thomas.cuesta31@gmail.com>
2025-02-07 21:36:16 +0100

    refactor(components): Update contact page with Hero and CommunFaq components

--- assets/pages/contact.tsx
+++ assets/pages/contact.tsx
@@ -1,15 +1,13 @@
 import React from "react";
+import CommunFaq from "../components/CommunFaq";
+import { Hero } from "../components/Hero";
 
 export default function Page() {
     return (
-        <div className="grow bg-dark-primary">
-            <header id="hero" className="relative">
-                <img src="/build/images/hero-home.png" alt="" className="object-cover w-screen" />
-                <h1 className="absolute top-1/2 right-1/2 text-secondary text-6xl font-bold translate-x-[50%] -translate-y-[50%]">
-                    Swiss Padel Stars
-                </h1>
-            </header>
-            <p>Welcome to the home page of this simple React app.</p>
+        <div className="grow bg-dark-primary space-y-10">
+            <Hero title="Formulaire de contact" img={{ src: "/build/images/hero-contact.png", alt: "banniere" }} />
+            <section className="space-y-10 w-full"></section>
+            <CommunFaq />
         </div>
     );
 }
