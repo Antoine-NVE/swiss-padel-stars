88277da67c525891c01ed03c74222a2df404bb00
razmiO <thomas.cuesta31@gmail.com>
2025-02-08 01:02:34 +0100

    refactor(ui): Update Dialog component styling in dialog.tsx

--- assets/pages/nous.tsx
+++ assets/pages/nous.tsx
@@ -3,6 +3,7 @@ import { Article } from "../components/Article";
 import CommunFaq from "../components/CommunFaq";
 import { Hero } from "../components/Hero";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../components/ui/carousel";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "../components/ui/dialog";
 import { type ImgProps } from "../types";
 
 export default function Page() {
@@ -20,19 +21,28 @@ export default function Page() {
 
     const carousel = [
         {
-            img: { src: "build/images/carousel-1.png", alt: "element 1 from the carousel", width: 325, height: 325 },
+            img: { src: "build/images/carousel-1.png", alt: "evenement 1 from the carousel", width: 325, height: 325 },
             link: "/",
-            label: "Element 1",
+            label: "Evenement 1",
+            description: [
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            ],
         },
         {
-            img: { src: "build/images/carousel-2.png", alt: "element 2 from the carousel", width: 325, height: 325 },
+            img: { src: "build/images/carousel-2.png", alt: "evenement 2 from the carousel", width: 325, height: 325 },
             link: "/",
-            label: "Element 2",
+            label: "Evenement 2",
+            description: [
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            ],
         },
         {
-            img: { src: "build/images/carousel-3.png", alt: "element 3 from the carousel", width: 325, height: 325 },
+            img: { src: "build/images/carousel-3.png", alt: "evenement 3 from the carousel", width: 325, height: 325 },
             link: "/",
-            label: "Element 3",
+            label: "Evenement 3",
+            description: [
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            ],
         },
     ];
 
@@ -68,6 +78,7 @@ export default function Page() {
                     </p>
                 </div>
             </section>
+
             <section className="flex flex-col items-center justify-center gap-10">
                 <h3 className="text-3xl text-secondary font-semibold">Evenement Blog</h3>
                 <Carousel opts={{ align: "start", loop: true, startIndex: 1 }} className="max-w-5xl">
@@ -79,9 +90,26 @@ export default function Page() {
                                         key={index}
                                         className="basis-1/3 flex flex-col items-center justify-center gap-5">
                                         <img src={item.img.src} alt={item.img.alt} width={325} />
-                                        <a href={item.link} className="text-white text-3xl hover:underline">
-                                            {item.label}
-                                        </a>
+                                        <Dialog>
+                                            <DialogTrigger className="text-white text-3xl hover:underline">
+                                                {item.label}
+                                            </DialogTrigger>
+                                            <DialogContent className="bg-grey text-white overflow-y-scroll">
+                                                <div className="flex flex-col items-center justify-center gap-5">
+                                                    <img src={item.img.src} alt={item.img.alt} className="w-8/12" />
+                                                    <DialogTitle className="text-secondary text-2xl">
+                                                        {item.label}
+                                                    </DialogTitle>
+                                                    {item.description.map((desc, index) => (
+                                                        <DialogDescription
+                                                            key={index}
+                                                            className="text-white text-justify">
+                                                            {desc}
+                                                        </DialogDescription>
+                                                    ))}
+                                                </div>
+                                            </DialogContent>
+                                        </Dialog>
                                     </CarouselItem>
                                 ))
                             )
@@ -91,6 +119,7 @@ export default function Page() {
                     <CarouselNext className="text-secondary scale-[3] -translate-x-[100%]" variant={"ghost"} />
                 </Carousel>
             </section>
+
             <CommunFaq />
         </div>
     );
