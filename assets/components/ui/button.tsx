570c41936c5446949d2df5dc560387c10e2478eb
razmiO <thomas.cuesta31@gmail.com>
2025-02-09 20:05:48 +0100

    refactor(ui): merge custom styles and shadcn ones

--- assets/components/ui/button.tsx
+++ assets/components/ui/button.tsx
@@ -9,15 +9,16 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+                default: "bg-dark-secondary text-white shadow hover:bg-primary/90",
                 destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
                 outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
                 secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
+                carousel: "bg-transparent text-secondary hover:scale-[3.3] transition-all scale-[2.8]",
             },
             size: {
-                default: "h-9 px-4 py-2",
+                default: "h-9 px-4 py-2 rounded-3xl w-fit",
                 sm: "h-8 rounded-md px-3 text-xs",
                 lg: "h-10 rounded-md px-8",
                 icon: "h-9 w-9",
@@ -39,16 +40,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({ className, variant, size, asChild = false, ...props }, ref) => {
         const Comp = asChild ? Slot : "button";
-        return (
-            <Comp
-                className={cn(
-                    buttonVariants({ variant, size, className }),
-                    "bg-dark-secondary text-white py-2 px-4 rounded-3xl w-fit"
-                )}
-                ref={ref}
-                {...props}
-            />
-        );
+        return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
     }
 );
 Button.displayName = "Button";
