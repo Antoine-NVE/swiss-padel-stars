707a158b5abddd580446f8ff3b65b685ad31a165
razmiO <thomas.cuesta31@gmail.com>
2025-03-15 15:32:20 +0100

    fix : className was applied to img instead of parent <a>

--- assets/components/Logo.tsx
+++ assets/components/Logo.tsx
@@ -3,8 +3,8 @@ import { cn } from "../utils";
 
 export default function Logo({ className }: { className?: string }) {
     return (
-        <a href="/" aria-label="go to main page">
-            <img src={"/build/images/logo.png"} alt="Swiss Padel Stars logo" className={cn("h-10", className)} />
+        <a href="/" aria-label="go to main page" className={cn(className)}>
+            <img src={"/build/images/logo.png"} alt="Swiss Padel Stars logo" className={"h-10"} />
         </a>
     );
 }
