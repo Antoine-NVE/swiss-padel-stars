11fcbfd1f35d8cb60bff9270dc1c7485337abab3
Antoine-NVE <antoinenavette@mail.com>
2025-04-16 09:56:21 +0200

    feature: create article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Response\StandardJsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+#[Route('/api/article', name: 'api_article_')]
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/create', name: 'create', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function create(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        $data = json_decode($request->getContent(), true);
+
+        $article = new Article();
+        $article->setTitle($data['title'] ?? '');
+        $article->setDescription($data['description'] ?? '');
+        $article->setCreatedAt(new \DateTimeImmutable());
+
+        $errors = $validator->validate($article);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+            return StandardJsonResponse::error('Une erreur est survenue.', $errorMessages, 400);
+        }
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return StandardJsonResponse::success('Article créé', [
+            'id' => $article->getId(),
+            'title' => $article->getTitle(),
+            'description' => $article->getDescription(),
+            'createdAt' => $article->getCreatedAt()->format('Y-m-d H:i:s'),
         ]);
     }
 }
