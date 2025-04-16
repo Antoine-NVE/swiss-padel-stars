1661db1bec202f1ca86631eb005892aef9b556b8
Antoine-NVE <antoinenavette@mail.com>
2025-04-16 10:19:59 +0200

    feat: create article with images

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\ArticleImage;
 use App\Response\StandardJsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/article', name: 'api_article_')]
@@ -20,14 +23,33 @@ class ArticleController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function create(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        $title = $request->request->get('title', '');
+        $description = $request->request->get('description', '');
+        $imageFiles = $request->files->get('images'); // tableau d'images
 
         $article = new Article();
-        $article->setTitle($data['title'] ?? '');
-        $article->setDescription($data['description'] ?? '');
+        $article->setTitle($title);
+        $article->setDescription($description);
         $article->setCreatedAt(new \DateTimeImmutable());
 
         $errors = $validator->validate($article);
+
+        if ($imageFiles instanceof UploadedFile) {
+            $imageFiles = [$imageFiles];
+        }
+
+        // Vérifie qu'au moins une image est fournie
+        if (!$imageFiles || count($imageFiles) < 1) {
+            $errors->add(new ConstraintViolation(
+                'Veuillez ajouter au moins une image.',
+                null,
+                [],
+                '',
+                'images',
+                null
+            ));
+        }
+
         if (count($errors) > 0) {
             $errorMessages = [];
             foreach ($errors as $error) {
@@ -37,6 +59,20 @@ class ArticleController extends AbstractController
         }
 
         $entityManager->persist($article);
+
+        foreach ($imageFiles as $imageFile) {
+            if ($imageFile && $imageFile->isValid()) {
+                $fileName = uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move('uploads/articles', $fileName);
+
+                $articleImage = new ArticleImage();
+                $articleImage->setArticle($article);
+                $articleImage->setFileName('uploads/articles/' . $fileName);
+
+                $entityManager->persist($articleImage);
+            }
+        }
+
         $entityManager->flush();
 
         return StandardJsonResponse::success('Article créé', [
