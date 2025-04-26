293f308a7063164de9a23108d740cb16b643722e
Antoine-NVE <antoinenavette@gmail.com>
2025-04-26 13:00:16 +0100

    chore: migration

--- migrations/Version20250426115914.php
+++ migrations/Version20250426115914.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250416074919 extends AbstractMigration
+final class Version20250426115914 extends AbstractMigration
 {
     public function getDescription(): string
     {
