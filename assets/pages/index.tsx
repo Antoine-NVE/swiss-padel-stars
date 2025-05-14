69df6444f4897119095595c31af15ecdf336ac0f
Luc Houvrard <88780925+Luc-Houvrard@users.noreply.github.com>
2025-05-14 10:36:56 +0200

    feat: change text

--- assets/pages/index.tsx
+++ assets/pages/index.tsx
@@ -8,23 +8,24 @@ import { type ImgProps } from "../types";
 const articles: { img: ImgProps | ImgProps[]; text: string; btnText: string }[] = [
     {
         img: { src: "build/images/index/body-1.png", alt: "", width: 500, height: 400 },
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-        btnText: "Ghost",
+        text: "Swiss Padel Stars accompagne les entreprises, les clubs et les collectivités dans le développement du padel en Suisse romande – de l'implantation d'infrastructures à l'organisation d'événements, en passant par le conseil en sponsoring et en logistique.",
+        btnText: "GHOST",
     },
     {
         img: { src: "build/images/index/body-2.png", alt: "", width: 500, height: 400 },
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-        btnText: "Contact",
+        text: "Accessible, moderne et en forte croissance, le padel offre des opportunités de sponsoring uniques, avec une audience engagée et encore loin des tarifs prohibitifs du tennis ou des sports automobiles. Nous aidons marques, clubs et institutions à s'emparer de cet engouement",
+        btnText: "CONTACT",
     },
     {
         img: [
             { src: "build/images/index/body-grid-1.png", alt: "", width: 220, height: 220 }, //
             { src: "build/images/index/body-grid-2.png", alt: "", width: 220, height: 220 }, //
         ],
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-        btnText: "Commander un devis",
+        text: "Swiss Padel Stars est né d'une conviction simple : le padel n'est pas qu'un sport en plein essor, c'est un formidable terrain d'opportunités. Pour créer des expériences, des partenariats et des lieux de vie uniques, vous voulez construire un terrain?",
+        btnText: "DEMANDEZ VOTRE DEVIS",
     },
 ];
+
 export default function Page() {
     return (
         <>
